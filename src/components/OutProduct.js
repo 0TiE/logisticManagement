f605647e3c6ab6d2123a8cbb47257664ef002621
Ravidu Theekshana <125554585+0TiE@users.noreply.github.com>
2024-07-11 10:55:55 +0530

    rfc

---
+++ src/components/OutProduct.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OutProduct() {
+  return (
+    <div>OutProduct</div>
+  )
+}
---
+++ src/components/StockManageme.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StockManageme() {
+  return (
+    <div>StockManageme</div>
+  )
+}
